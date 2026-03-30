0a143b24b7a35eb3201c50ca45414fb31ad7be60
taji <taji@sonicgarden.jp>
2026-03-30 22:30:23 +0900

    chakra-ui install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@chakra-ui/react"
+
 function App() {
   return (
     <>
       <h1>Hello Nakajima</h1>
+      <Button colorScheme="blue">Click me</Button>
     </>
   )
 }
