2688efe17c4b6f3eb7aede3ab7b9e11375c50138
Takumi Tajima <taji@sonicgarden.jp>
2026-04-26 22:15:31 +0900

    class定義

--- src/StudyRecords.tsx
+++ src/StudyRecords.tsx
@@ -1,15 +1,19 @@
 import { supabase } from './supabaseClient'
 import { useEffect, useState } from 'react'
+import { Record } from './domain/record'
 
 export const  StudyRecords = () => {
-  const [records, setRecords] = useState([])
+  const [records, setRecords] = useState<Record[]>([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchRecords = async () => {
       const { data, error } = await supabase.from('study-record').select('*')
       if (error) console.error(error)
-      else setRecords(data)
+      else if (data) {
+        const recordList: Record[] = data.map((row) => new Record(row.id, row.title, row.time))
+        setRecords(recordList)
+      }
       setLoading(false)
     }
     fetchRecords()
